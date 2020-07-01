f3548599109ccbdd972d4357dc896ca1907f148a
Michal <ga>
2020-07-01 21:26:34 +0200

    dodanie polaczenia z baza

---
+++ src/main/java/pl/kancelaria/AHG/AhgApplication.java
@@ -0,0 +1,13 @@
+package pl.kancelaria.AHG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kancelaria/AHG/AhgApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kancelaria.AHG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
