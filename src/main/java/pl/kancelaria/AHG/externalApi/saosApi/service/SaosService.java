a98a3149058a7a303f98f17077e0847d559d36ef
Michał Gajewski <michal.gajewski@zetosoftware.pl>
2023-09-14 15:30:19 +0200

    fix comments

--- src/main/java/pl/kancelaria/AHG/externalApi/saosApi/service/SaosService.java
+++ src/main/java/pl/kancelaria/AHG/externalApi/saosApi/service/SaosService.java
@@ -2,9 +2,7 @@ package pl.kancelaria.AHG.externalApi.saosApi.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.net.URI;
@@ -12,14 +10,10 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SaosService {
-
-//    private final RestTemplate restTemplate;
 
     @Value("${saosApi10}")
     private String saosHost;
@@ -33,15 +27,6 @@ public class SaosService {
                 .GET()
                 .build();
 
-        return  httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
-
-//        HttpHeaders headers = new HttpHeaders();
-//        List<MediaType> mediaTypes = new ArrayList<>();
-//        mediaTypes.add(MediaType.APPLICATION_JSON);
-//        headers.setAccept(mediaTypes);
-//
-//        RestTemplate restTemplate =  new RestTemplate();
-//
-//       return restTemplate.exchange(saosHost, HttpMethod.GET, new HttpEntity<String>(headers), String.class);
+        return httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
     }
 }
